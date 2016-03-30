318e412cc2d49f59d2fdcdb596253d64ea3e8d13
Jason Rametta <rametta@outlook.com>
2016-03-30 10:48:23 -0400

    Template changes

--- gallery/forms.py
+++ gallery/forms.py
@@ -2,11 +2,10 @@ from django import forms
 from gallery.models import Work
 
 class WorkForm(forms.ModelForm):
-    
     class Meta:
         model = Work
         fields = (
-            'title', 
+            'title',
             'description',
             'image',
         )
