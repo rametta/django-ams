9560c5b51149e60f1744c128885218763e7468cf
Jason Rametta <rametta@outlook.com>
2016-03-30 16:05:44 -0400

    added more stuff

--- gallery/views.py
+++ gallery/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.models import User
 from gallery.models import Work
-from gallery.forms import WorkForm
+import gallery.forms
+
 
 def index(request):
-    return render(request, 'index.html')
+    # SELECT * FROM gallery_work WHERE status = 'approved' ORDER BY submit_date DESC LIMIT 3
+    approved_work = Work.objects.filter(status='approved').order_by('-submit_date')[:3]
+    return render(request, 'index.html', {'approved': approved_work})
+
 
 def work(request):
-    #SELECT * FROM gallery_work WHERE status = 'approved'
+    # SELECT * FROM gallery_work WHERE status = 'approved'
     approved_work = Work.objects.filter(status='approved')
-    return render(request, 'work.html', {'approved': approved_work,})
+    return render(request, 'work.html', {'approved': approved_work})
+
 
 def view_work(request, id):
     if request.method == 'POST':
@@ -22,37 +28,42 @@ def view_work(request, id):
             w.reject()
         return redirect('view_work', id=id)
     elif request.method == 'GET':
-        #SELECT * FROM gallery_work WHERE id = id
+        # SELECT * FROM gallery_work WHERE id = id
         work = Work.objects.get(id=id)
-        return render(request, 'view_work.html', {'work': work,})
+        return render(request, 'view_work.html', {'work': work})
+
 
 def artists(request):
-    #SELECT * FROM auth_user ORDER BY id ASC
+    # SELECT * FROM auth_user ORDER BY id ASC
     all_artists = User.objects.all().order_by('id')
-    return render(request, 'artists.html', {'all_artists': all_artists,})
+    return render(request, 'artists.html', {'all_artists': all_artists})
+
 
 def view_artist(request, id):
-    #SELECT * FROM auth_user WHERE username = username
+    # SELECT * FROM auth_user WHERE username = username
     artist = User.objects.get(id=id)
-    #SELECT * FROM gallery_work WHERE artist = id
+    # SELECT * FROM gallery_work WHERE artist = id
     work = Work.objects.filter(artist=id, status='approved')
-    return render(request, 'view_artist.html', {'artist': artist, 'work': work,})
+    return render(request, 'view_artist.html', {'artist': artist, 'work': work})
+
 
 @login_required
 def add_work(request):
     if request.method == 'POST':
-        form = WorkForm(data=request.POST, files=request.FILES)
+        form = gallery.forms.WorkForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             work = form.save(commit=False)
             work.artist = request.user
             work.save()
             return redirect('work')
     else:
-        form = WorkForm()
-        
+        form = gallery.forms.WorkForm()
+
     return render(request, 'add_work.html', {'form': form})
 
+
+@staff_member_required
 def staff(request):
-    #SELECT * FROM gallery_work WHERE status = 'pending'
+    # SELECT * FROM gallery_work WHERE status = 'pending'
     work = Work.objects.filter(status = 'pending')
-    return render(request, 'staff.html', {'pending': work,})
+    return render(request, 'staff.html', {'pending': work})
