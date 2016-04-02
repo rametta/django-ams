6bb60dd3cb9719e98c59c6591abfeb61fc76c9ca
Jason Rametta <rametta@outlook.com>
2016-04-01 23:43:38 -0400

    configured for amazon aws s3 media storage

--- collection/settings_production.py
+++ collection/settings_production.py
@@ -1,8 +1,17 @@
 from collection.settings import *
 import dj_database_url
 
+AWS_STORAGE_BUCKET_NAME = 'galleryx'
+AWS_ACCESS_KEY_ID = 'AKIAI7TAAKTCHYX3GNIA'
+AWS_SECRET_ACCESS_KEY = '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'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 DEBUG = False
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
