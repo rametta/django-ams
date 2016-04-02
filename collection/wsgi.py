072328fdfc87ab4710bb48efd8dbbab142d91123
Jason Rametta <rametta@outlook.com>
2016-04-01 20:50:55 -0400

    production settings added for heroku

--- collection/wsgi.py
+++ collection/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collection.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collection.settings_production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
